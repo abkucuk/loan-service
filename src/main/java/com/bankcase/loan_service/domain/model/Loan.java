64e38b62c0844c62ed5ca362018e68bc8aa40d3e
ahmetbkucuk <ahmetbkucuk@gmail.com>
2025-11-18 21:30:54 +0300

    refactor Loan class to use value objects and manage installments

--- src/main/java/com/bankcase/loan_service/domain/model/Loan.java
+++ src/main/java/com/bankcase/loan_service/domain/model/Loan.java
@@ -3,41 +3,122 @@ package com.bankcase.loan_service.domain.model;
 import com.bankcase.loan_service.domain.model.enums.LoanStatus;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import com.bankcase.loan_service.infra.repository.LoanInstallmentEntity;
 import lombok.Getter;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 public class Loan {
 
-    private Long id;
-    private Long customerId;
-    private BigDecimal loanAmount;
-    private BigDecimal remainingAmount;
-    private BigDecimal interestRate;
-    private int termInMonths;
+    private final Long id;
+    private final Long customerId;
+    private Money amount;
+    private InterestRate interestRate;
+    private NumberOfInstallment numberOfInstallment;
     private LoanStatus status;
     private LocalDateTime createdAt;
+    private List<LoanInstallment> installments;
 
-    public Loan(Long id,
-                Long customerId,
-                BigDecimal loanAmount,
-                BigDecimal interestRate,
-                int termInMonths,
-                LoanStatus status,
-                LocalDateTime createdAt) {
 
+    private static final List<Integer> VALID_INSTALLMENTS = List.of(6,9,12,24);
+
+    private Loan(
+            Long id,
+            Long customerId,
+            Money amount,
+            InterestRate interestRate,
+            NumberOfInstallment numberOfInstallment,
+            LoanStatus status,
+            LocalDateTime createdAt,
+            List<LoanInstallment> installments
+    ) {
         this.id = id;
         this.customerId = customerId;
-        this.loanAmount = loanAmount;
-        this.remainingAmount = loanAmount;
+        this.amount = amount;
         this.interestRate = interestRate;
-        this.termInMonths = termInMonths;
-        this.status = status != null ? status : LoanStatus.PENDING;
-        this.createdAt = createdAt != null ? createdAt : LocalDateTime.now();
+        this.numberOfInstallment = numberOfInstallment;
+        this.status = status;
+        this.createdAt = createdAt;
+        this.installments = installments;
+    }
+
+    public static Loan create(
+            Long id,
+            Long customerId,
+            Money amount,
+            InterestRate interestRate,
+            NumberOfInstallment numberOfInstallment
+    ) {
+        if (customerId == null) {
+            throw new IllegalArgumentException("Customer ID cannot be null");
+        }
+
+        if (amount == null) {
+            throw new IllegalArgumentException("Amount cannot be null");
+        }
+
+        if (interestRate == null) {
+            throw new IllegalArgumentException("Interest rate cannot be null");
+        }
+
+        if (numberOfInstallment == null) {
+            throw new IllegalArgumentException("Installments cannot be null");
+        }
+
+        BigDecimal totalAmount = amount.getValue()
+                .multiply(BigDecimal.ONE.add(interestRate.getValue()));
+
+        BigDecimal installmentAmount = totalAmount
+                .divide(BigDecimal.valueOf(numberOfInstallment.getValue()), 2, RoundingMode.HALF_UP);
+
+        List<LoanInstallment> installments = createInstallments(numberOfInstallment, installmentAmount);
+
+
+        if (installmentAmount.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalStateException("Installment amount must be positive.");
+        }
+
+        return new Loan(
+                id,
+                customerId,
+                amount,
+                interestRate,
+                numberOfInstallment,
+                LoanStatus.APPROVED,
+                LocalDateTime.now(),
+                installments
+        );
+    }
+
+    private static List<LoanInstallment> createInstallments(NumberOfInstallment numberOfInstallment, BigDecimal installmentAmount) {
+        List<LoanInstallment> installments = new ArrayList<>();
+        LocalDate nextDueDate = generateNextDueDate(LocalDate.now());
+        for (int i = 0; i < numberOfInstallment.getValue(); i++) {
+            installments.add(
+                    LoanInstallment.create(
+                            null,
+                            null,
+                            Money.of(installmentAmount),
+                            nextDueDate
+                    )
+            );
+            nextDueDate = generateNextDueDate(nextDueDate);
+        }
+        return installments;
+    }
+
+    public static LocalDate generateNextDueDate(LocalDate baseDate) {
+        if (baseDate == null) {
+            throw new IllegalArgumentException("baseDate cannot be null");
+        }
+        LocalDate nextMonth = baseDate.plusMonths(1);
+        return nextMonth.withDayOfMonth(1);
     }
 
     public void approve() {
@@ -52,19 +133,5 @@ public class Loan {
             throw new IllegalStateException("Only PENDING loans can be rejected.");
         }
         this.status = LoanStatus.REJECTED;
-    }
-
-    public void applyPayment(BigDecimal amount) {
-        if (this.status != LoanStatus.APPROVED) {
-            throw new IllegalStateException("Payment can only be applied to APPROVED loans.");
-        }
-        if (amount.compareTo(BigDecimal.ZERO) <= 0) {
-            throw new IllegalArgumentException("Payment amount must be positive.");
-        }
-        this.remainingAmount = this.remainingAmount.subtract(amount);
-
-        if (this.remainingAmount.compareTo(BigDecimal.ZERO) <= 0) {
-            this.remainingAmount = BigDecimal.ZERO;
-        }
     }
 }
